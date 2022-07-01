857623b3a23286a8ea33f85116a88e964bd82e6e
KimSeungJin <dahyun101107@naver.com>
2022-07-01 10:28:50 +0900

    Create 루시와 엘라 찾기.sql

---
+++ String, Date/루시와 엘라 찾기.sql
@@ -0,0 +1,8 @@
+## 방법 1
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS 
+WHERE NAME = 'Lucy' OR NAME = 'Ella' OR NAME = 'Pickle' OR NAME = 'Rogan' OR NAME = 'Sabrina' OR NAME = 'Mitty'
+ORDER BY ANIMAL_ID
+
+## 방법 2
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS 
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
